8c948083ab9d68601fcdb85afe12f7f6d22f2715
agfalcon <agfalcon12@gmail.com>
2023-01-28 19:45:58 +0900

    단위 변환 기능 구현

--- app/src/main/java/kgb/toyproject/unitconverter/MainActivity.kt
+++ app/src/main/java/kgb/toyproject/unitconverter/MainActivity.kt
@@ -2,6 +2,7 @@ package kgb.toyproject.unitconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
 import kgb.toyproject.unitconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var inputNumber : Int = 0
+        var cmToM = true
+
+        binding.editNumber.addTextChangedListener { text ->
+            inputNumber = if(text.isNullOrEmpty()) 0
+            else text.toString().toInt()
+            if(cmToM){
+                binding.textNumber.text = inputNumber.times(0.01).toString()
+            } else{
+                binding.textNumber.text = inputNumber.times(100).toString()
+            }
+
+        }
+
+        binding.btnSwap.setOnClickListener {
+            cmToM = !cmToM
+            if(cmToM){
+                binding.textInputUnit.text="cm"
+                binding.textOutputUnit.text="m"
+                binding.textNumber.text = inputNumber.times(0.01).toString()
+            }else{
+                binding.textInputUnit.text="m"
+                binding.textOutputUnit.text="cm"
+                binding.textNumber.text = inputNumber.times(100).toString()
+            }
+        }
     }
 }
